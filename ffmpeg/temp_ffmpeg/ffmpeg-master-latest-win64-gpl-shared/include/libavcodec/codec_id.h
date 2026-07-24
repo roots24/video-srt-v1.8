4e01251f1d9a7db6d25ad0bb7357a5a04fe77ba5
roots24 <35600203+roots24@users.noreply.github.com>
2026-07-24 22:51:01 +0200

    Revert "Add raw color parameters support for ProRes RAW"
    This reverts commit 5827b3259ac470f4ba7f91c988d2befc9675bf0b.

--- ffmpeg/temp_ffmpeg/ffmpeg-master-latest-win64-gpl-shared/include/libavcodec/codec_id.h
+++ ffmpeg/temp_ffmpeg/ffmpeg-master-latest-win64-gpl-shared/include/libavcodec/codec_id.h
@@ -24,6 +24,8 @@
 #include "libavutil/avutil.h"
 #include "libavutil/samplefmt.h"
 
+#include "version_major.h"
+
 /**
  * @addtogroup lavc_core
  * @{
@@ -204,6 +206,9 @@ enum AVCodecID {
     AV_CODEC_ID_BMV_VIDEO,
     AV_CODEC_ID_VBLE,
     AV_CODEC_ID_DXTORY,
+#if FF_API_V408_CODECID
+    AV_CODEC_ID_V410,
+#endif
     AV_CODEC_ID_XWD,
     AV_CODEC_ID_CDXL,
     AV_CODEC_ID_XBM,
@@ -251,6 +256,10 @@ enum AVCodecID {
     AV_CODEC_ID_012V,
     AV_CODEC_ID_AVUI,
     AV_CODEC_ID_TARGA_Y216,
+#if FF_API_V408_CODECID
+    AV_CODEC_ID_V308,
+    AV_CODEC_ID_V408,
+#endif
     AV_CODEC_ID_YUV4,
     AV_CODEC_ID_AVRN,
     AV_CODEC_ID_CPIA,
@@ -558,7 +567,6 @@ enum AVCodecID {
     AV_CODEC_ID_LC3,
     AV_CODEC_ID_G728,
     AV_CODEC_ID_AHX,
-    AV_CODEC_ID_APPLE_APAC,
 
     /* subtitle codecs */
     AV_CODEC_ID_FIRST_SUBTITLE = 0x17000,          ///< A dummy ID pointing at the start of subtitle codecs.
--- ffmpeg/temp_ffmpeg/ffmpeg-master-latest-win64-gpl-shared/include/libavfilter/avfilter.h
+++ ffmpeg/temp_ffmpeg/ffmpeg-master-latest-win64-gpl-shared/include/libavfilter/avfilter.h
@@ -37,6 +37,7 @@
 
 #include <stddef.h>
 
+#include "libavutil/attributes.h"
 #include "libavutil/avutil.h"
 #include "libavutil/buffer.h"
 #include "libavutil/dict.h"
@@ -314,7 +315,26 @@ typedef struct AVFilterContext {
      */
     int nb_threads;
 
+#if FF_API_CONTEXT_PUBLIC
+    /**
+     * @deprecated unused
+     */
+    attribute_deprecated
+    struct AVFilterCommand *command_queue;
+#endif
+
     char *enable_str;               ///< enable expression string
+#if FF_API_CONTEXT_PUBLIC
+    /**
+     * @deprecated unused
+     */
+    attribute_deprecated
+    void *enable;
+    /**
+     * @deprecated unused
+     */
+    double *var_values;
+#endif
     /**
      * MUST NOT be accessed from outside avfilter.
      *
@@ -334,6 +354,14 @@ typedef struct AVFilterContext {
      * avfilter_init_dict().
      */
     AVBufferRef *hw_device_ctx;
+
+#if FF_API_CONTEXT_PUBLIC
+    /**
+     * @deprecated this field should never have been accessed by callers
+     */
+    attribute_deprecated
+    unsigned ready;
+#endif
 
     /**
      * Sets the number of extra hardware frames which the filter will
